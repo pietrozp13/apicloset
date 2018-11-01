0a27028ee4d58936b6c8e8e7eda5501e15ec6a6f
PIetro Perboni <pietroperboni@pis-macbook-pro.localdomain>
2018-11-01 00:47:36 -0300

    fix kiroku 02

--- lib/apicloset_web/channels/user_socket.ex
+++ lib/apicloset_web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule ApiclosetWeb.UserSocket do
   # channel "room:*", ApiclosetWeb.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
   timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
