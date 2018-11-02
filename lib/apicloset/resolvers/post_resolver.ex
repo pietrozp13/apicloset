d2f1b6947474cee622800c62a38b73e967fe476d
PIetro Perboni <pietroperboni@pis-macbook-pro.localdomain>
2018-11-02 01:42:08 -0300

    graphQL mutation post

--- lib/apicloset/resolvers/post_resolver.ex
+++ lib/apicloset/resolvers/post_resolver.ex
@@ -4,4 +4,15 @@ defmodule Apicloset.PostResolver do
   def all(_args, _info) do
     {:ok, Posts.list_posts()}
   end
+
+  def create(args, _info) do
+    Posts.create_post(args)
+  end
+
+  def find(%{id: id}, _info) do
+    case Posts.get_post!(id) do
+      nil -> {:error, "Post id #{id} not found"}
+      post -> {:ok, post}
+    end
+  end
 end
--- lib/apicloset_web/schema.ex
+++ lib/apicloset_web/schema.ex
@@ -7,6 +7,11 @@ defmodule ApiclosetWeb.Schema do
       resolve &Apicloset.PostResolver.all/2
     end
 
+    field :post, type: :post do
+      arg :id, non_null(:id)
+      resolve &Apicloset.PostResolver.find/2
+    end
+
     field :users, list_of(:user) do
       resolve &Apicloset.UserResolver.all/2
     end
@@ -14,6 +19,16 @@ defmodule ApiclosetWeb.Schema do
     field :user, type: :user do
       arg :id, non_null(:id)
       resolve &Apicloset.UserResolver.find/2
+    end
+  end
+
+  mutation do
+    field :create_post, type: :post do
+      arg :title, non_null(:string)
+      arg :body, non_null(:string)
+      arg :user_id, non_null(:integer)
+
+      resolve &Apicloset.PostResolver.create/2
     end
   end
 end
